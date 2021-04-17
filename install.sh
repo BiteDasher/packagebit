4a0543774aef11fc8b32d70f411c353f1372e653
BiteDasher <cazorax@mail.ru>
2021-04-17 15:40:11 +0300

    add install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+if [ "$(id -u)" -ne 0 ]; then
+	echo "sudo required"
+	exit 1
+fi
+install -D -m 755 lint /usr/local/share/PackageBit/lint
+install -D -m 755 pbcon /usr/bin/pbcon
+for backend in *.backend; do
+	install -D -m 755 $backend /usr/local/share/PackageBit/$backend
+done
