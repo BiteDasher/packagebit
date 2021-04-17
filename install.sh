3d5948e2e204431947e46c4d5ed54c930cfe9374
BiteDasher <cazorax@mail.ru>
2021-04-17 15:41:03 +0300

    755 -> 644

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ if [ "$(id -u)" -ne 0 ]; then
 	echo "sudo required"
 	exit 1
 fi
-install -D -m 755 lint /usr/local/share/PackageBit/lint
+install -D -m 644 lint /usr/local/share/PackageBit/lint
 install -D -m 755 pbcon /usr/bin/pbcon
 for backend in *.backend; do
-	install -D -m 755 $backend /usr/local/share/PackageBit/$backend
+	install -D -m 644 $backend /usr/local/share/PackageBit/$backend
 done
